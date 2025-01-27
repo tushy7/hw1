da980896bcc8a5eae9fe153f576e6e2ad1a76cc6
Tushar Zhade <zhade@usc.edu>
2025-01-27 11:06:56 -0800

    question 1

--- split.cpp
+++ split.cpp
@@ -11,13 +11,38 @@ the function below should be the only one in this file.
 */
 
 #include "split.h"
-
 /* Add a prototype for a helper function here if you need */
+void append(Node*& in, Node*& eveOdd);
 
-void split(Node*& in, Node*& odds, Node*& evens)
-{
-  /* Add code here */
-// WRITE YOUR CODE HERE
+
+void split(Node*& in, Node*& odds, Node*& evens) {
+  if (in == nullptr){
+    return;
+  }
+  if(in -> value % 2 == 0) {
+    append(in, evens);
+  }
+  else {
+    append(in, odds);
+  }
+  return split(in, odds, evens);
 }
 
 /* If you needed a helper function, write it here */
+void append(Node*& in, Node*& eveOdd) {
+  Node* tmp = in -> next; 
+
+  if (eveOdd == nullptr) { //check if even/odd are set to NULL (meaning they are empty)
+    eveOdd = in; //set even/odd linked list to proper node, which "in" is currently pointing to
+    eveOdd -> next = nullptr; //make the added nodes "next" pointer, point to null meaning it doesn't point to anything else
+  }
+  else {
+    append(in, eveOdd->next); //find the last element in the even/odd linked list
+    return;
+    
+    eveOdd -> next = in; //make the current last element's "next" pointer, point to what "in" points to 
+    eveOdd = eveOdd -> next; //go to the next node
+    eveOdd -> next = nullptr; //set next node's "next" pointer to null so it doesn't point to anything else
+  }
+  in = tmp; //after moving the node "in" points to, move in to the next node
+}
